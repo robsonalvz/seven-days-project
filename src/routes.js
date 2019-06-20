3ddf28a9d15a8af35345f9580d638c6bc076727c
robsonalvz <robson.alves@dce.ufpb.br>
2019-06-20 03:41:51 -0300

    end firts day

--- src/routes.js
+++ src/routes.js
@@ -3,21 +3,21 @@ const routes = new express.Router();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const UserController = require('./controllers/UserController')
-const users = [{ id: '242424', email: 'test@test.com', password: 'password'}]
+//const users = [{ id: '242424', email: 'test@test.com', password: 'password'}]
 
 // configure passport.js to use the local strategy
 passport.use(new LocalStrategy(
     {usernameField: 'email'},
     (email, password, done) => {
         console.log('Inside local strategy callback')
-        // here is where you make a call to the database
-        // to find the user based on their username or email address
-        // for now, we'll just pretend we found that it was users[0]
-        const user = users[0] 
-        if (email == user.email && password == user.password ){
-            console.log('Local strategy return true')
-            return done(null,user)
-        }
+        UserController.findByEmail(email)
+        .then(user => {
+            if (email == user.email && password == user.password ){
+                console.log('Local strategy return true')
+                return done(null,user)
+            }
+        });
+
     }
     ));
     
@@ -54,13 +54,9 @@ routes.get('/login', (req,res)=>{
 routes.post('/login', (req,res, next)=>{
     console.log('Inside POST/ login callback function');
     passport.authenticate('local', (err, user, info)=>{
-        console.log('Inside passport.authenticate() callback');
-        console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
-        console.log(`req.user: ${JSON.stringify(req.user)}`)
+      
         req.login(user, (err)=>{
-            console.log('Inside req.login() callback')
-            console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
-            console.log(`req.user: ${JSON.stringify(req.user)}`)
+          
             return res.send('You were authenticated & logged in!\n');
         })
     })(req, res, next);
