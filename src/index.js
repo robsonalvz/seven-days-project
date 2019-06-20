518d6cec368f7bf1f3b4c7d745d4b78ae4dd7d7b
robsonalvz <robson.alves@dce.ufpb.br>
2019-06-20 00:37:19 -0300

    add user session

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 /** Cors Allow all applications types to access backend */
 const cors = require('cors');
+const session = require('express-session');
+const uuid = require('uuid/v4');
+const FileStore = require('session-file-store')(session);
 
 const app = express();
 
+app.use(session({
+    genid:(req)=>{
+        console.log('Inside the session on the middleware')
+        console.log(req.sessionID)
+        return uuid() // return UUIDs for session IDs
+    },
+    store:new FileStore(),
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized:true,
+}))
 app.use(require('./routes'));
 
 app.use(cors());
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const uuid = require('uuid/v4');
 const routes = new express.Router();
 
 
 routes.get('/', (req,res)=>{
-    console.log(req)
-    const uniqueId = uuid();
-    res.send(`Bem vindo a tela inicial!: ${uniqueId}\n`);
-
+    console.log('Inside the homepage callback function');
+    console.log(req.sessionID);
+    res.send(`Bem vindo a tela inicial!`);
 })
 
 module.exports = routes;
