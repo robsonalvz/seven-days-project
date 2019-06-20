8257af23d0a4a938fc70a851262c787e18318b3c
robsonalvz <robson.alves@dce.ufpb.br>
2019-06-19 22:44:28 -0300

    add mongoose and cors dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-const express = require('express')
+const express = require('express');
+/** mongoose for mongodb atlas */
+const mongoose = require('mongoose');
+/** Cors Allow all applications types to access backend */
+const cors = require('cors');
+const app = express();
 
-const app = express()
+
+app.use(cors());
+
 
 app.get('/', (req,res)=>{
     res.send('Bem vindo a tela inicial!');
